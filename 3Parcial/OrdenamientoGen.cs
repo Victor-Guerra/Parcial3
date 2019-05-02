4fff72d68d1cb1a1e9ad30624ea9beb61899ed42
Victor_Guerra <a01568075@itesm.mx>
2019-05-02 13:31:20 -0600

    d

--- 3Parcial/OrdenamientoGen.cs
+++ 3Parcial/OrdenamientoGen.cs
@@ -40,7 +40,7 @@ arr[j + 1] = key;
                 key = arr[i];
                 j = i - 1;
 
-                while(j >= 0 && ()
+                while(j >= 0 && arr[j] > key)
                 {
                     arr[j + 1] = arr[j];
                     j--;
